fb06eb92282320b9f8c7b99f14394cb1d98e1cfe
online507 <joseardines03@gmail.com>
2024-06-10 11:42:04 -0500

    estudiante

---
+++ VisitanteOriginal/estudiantes.java
@@ -0,0 +1,21 @@
+package VisitanteOriginal;
+
+public class estudiantes {
+        public static void main(String[] args) {
+            // Declarar y asignar valores a variables
+            String nombre = "Juan Pérez";
+            int edad = 20;
+            String carrera = "Ingeniería Informática";
+            double promedio = 8.5;
+            String universidad = "Universidad Nacional";
+    
+            // Imprimir valores en pantalla
+            System.out.println("Información del Estudiante");
+            System.out.println("---------------------------");
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Edad: " + edad);
+            System.out.println("Carrera: " + carrera);
+            System.out.println("Promedio: " + promedio);
+            System.out.println("Universidad: " + universidad);
+        }
+    }
