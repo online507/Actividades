8a8a28564ea74d59038566cd19047344b65c5fe8
online507 <joseardines03@gmail.com>
2024-06-12 08:36:47 -0500

    subiendo parcial

---
+++ modulo5/parcial1.java
@@ -0,0 +1,50 @@
+public class parcial1{
+    private String name;
+    private int initialQuantity;
+    private int soldQuantity;
+
+    public parcial1(String name, int initialQuantity) {
+        this.name = name;
+        this.initialQuantity = initialQuantity;
+        this.soldQuantity = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getInitialQuantity() {
+        return initialQuantity;
+    }
+
+    public int getSoldQuantity() {
+        return soldQuantity;
+    }
+
+    public void sell(int quantity) {
+        this.soldQuantity += quantity;
+    }
+
+    public int getAvailableQuantity() {
+        return initialQuantity - soldQuantity;
+    }
+
+    public boolean isOutOfStock() {
+        return getAvailableQuantity() == 0;
+    }
+
+    public void duplicateInventory() {
+        this.initialQuantity *= 2;
+        this.soldQuantity = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", initialQuantity=" + initialQuantity +
+                ", soldQuantity=" + soldQuantity +
+                ", availableQuantity=" + getAvailableQuantity() +
+                '}';
+    }
+}
